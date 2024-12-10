ef24d4d76b7c595939bdd43733bae626d0deaee7
Bupik <bupikheadache@gmail.com>
2024-12-10 14:36:48 +0300

    Change BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -38,12 +39,16 @@ public class BaseTest {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--disable-notifications");  //отключение всплывающих уведомлений
             options.addArguments("start-maximized");
+            options.addArguments("headless");
             driver = new ChromeDriver(options);   //инициализация
             //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         } else if(browser.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions();
+            options.addArguments("headless");
+            driver = new EdgeDriver(options);
         } else if (browser.equalsIgnoreCase("firefox")) {
             FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("headless");
             driver = new FirefoxDriver(options);
         }
 
@@ -59,6 +64,8 @@ public class BaseTest {
     @Description("Закрытие браузера")
     public void tearDown(ITestResult result) {
         log.info("Close browser");
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
